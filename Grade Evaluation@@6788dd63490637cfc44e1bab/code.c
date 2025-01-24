991adc709dd59cab5cc30e46f9c43f128ec45714
ManpreetSinghGrewal <154874631+ManpreetSinghGrewal@users.noreply.github.com>
2025-01-24 16:11:33 +0530

    Solution to Grade Evaluation

---
+++ Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+char* welcome() {
+    return "Welcome to Code Radar!";
+}
+
+int main() {
+    char a;
+    scanf("%d",&a);
+    switch(a){
+        case 'A':
+        printf("Excellent");
+        case 'B':
+        printf("Good");
+        case 'C';
+        printf("Average");
+        case 'D';
+        printf("Below Average");
+    }
+    return 0;
+}
