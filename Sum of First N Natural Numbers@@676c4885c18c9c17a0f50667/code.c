631ab471e4e322e4355d359d1b2aafab6f26d715
ManpreetSinghGrewal <154874631+ManpreetSinghGrewal@users.noreply.github.com>
2025-01-27 17:07:40 +0530

    Solution to Sum of First N Natural Numbers

---
+++ Sum of First N Natural Numbers@@676c4885c18c9c17a0f50667/code.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+char* welcome() {
+    return "Welcome to Code Radar!";
+}
+
+int main() {
+    int n;
+    scanf("%d",&n);
+    int sum =0;
+    for(int  i=1;i<=n;i++){
+        sum = sum + 1;
+
+    }
+    return 0;
+}
