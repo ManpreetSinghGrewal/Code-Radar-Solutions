dadf5ef17245219dcbab3780afffe7386cc9b4d8
ManpreetSinghGrewal <154874631+ManpreetSinghGrewal@users.noreply.github.com>
2025-01-30 19:05:26 +0530

    Solution to Reverse a String

--- Reverse a String@@676c039d80e1714600f67640/code.c
+++ Reverse a String@@676c039d80e1714600f67640/code.c
@@ -10,10 +10,11 @@ int main() {
     char ch[90];
     int k = 89;
     for(int i= 0;i<=89;i++){
-        
+        if(s[i] == '\0')break;
         ch[k] = s[i];
         k-- ;
     }
+    
     printf("%89s",ch);
 
     return 0;
