bf4a37296fec89204eb3bfa3ce7c740bf77e98a9
ManpreetSinghGrewal <154874631+ManpreetSinghGrewal@users.noreply.github.com>
2025-02-04 00:59:06 +0530

    Solution to Calculate the Running Sum of an Array

--- Calculate the Running Sum of an Array@@676c02002dbc8c3e7a280b86/code.c
+++ Calculate the Running Sum of an Array@@676c02002dbc8c3e7a280b86/code.c
@@ -16,8 +16,7 @@ int main() {
     }
     int nea[n];
     nea[0] = num[0];
-    for(int j = 1;j<n;j++){
-        for(int k = 0 ; k<n;k++){
+    for(int j = 1;j<n;j++){ 
             nea[j] = num[j - 1] + num[j]; 
         }  
     } 
