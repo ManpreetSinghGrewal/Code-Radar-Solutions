932912fa8e94be060ef7044c9eec6ac9be1d42c5
ManpreetSinghGrewal <154874631+ManpreetSinghGrewal@users.noreply.github.com>
2025-02-03 23:13:19 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -21,9 +21,9 @@ int main() {
             else{
                 printf("error");
                 break; }
-        default:
-            printf("invalid operator");
-            break;
+        // default:
+        //     printf("invalid operator");
+        //     break;
     }
     return 0;
 }
