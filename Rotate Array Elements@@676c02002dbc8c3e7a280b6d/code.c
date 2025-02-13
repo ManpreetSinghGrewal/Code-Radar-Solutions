3bc21f2dc3aedddaee8e7d77c717fcd5ba911804
Manpreet Singh <154874631+ManpreetSinghGrewal@users.noreply.github.com>
2025-02-13 18:49:24 +0530

    Solution to Rotate Array Elements

--- Rotate Array Elements@@676c02002dbc8c3e7a280b6d/code.c
+++ Rotate Array Elements@@676c02002dbc8c3e7a280b6d/code.c
@@ -1,46 +1,38 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-void reverse(int arr[], int start, int end) {
-    while (start < end) {
-        int temp = arr[start];
-        arr[start] = arr[end];
-        arr[end] = temp;
-        start++;
-        end--;
+void rotateArray(int arr[], int n, int k) {
+    for (int i = 0; i < k; i++) {
+        int temp = arr[n - 1];
+        for (int j = n - 1; j > 0; j--) {
+            arr[j] = arr[j - 1];
+        }
+        arr[0] = temp;
     }
 }
 
-void rotateRight(int arr[], int n, int k) {
-    k = k % n; // In case k is larger than n
-    reverse(arr, 0, n - 1);
-    reverse(arr, 0, k - 1);
-    reverse(arr, k, n - 1);
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
 }
 
 int main() {
-    int z;
- 
-    scanf("%d", &z);
-    
-    int* arr = (int*)malloc(z * sizeof(int));
-    if (arr == NULL) {
-        printf("Memory not allocated.\n");
-        return 1;
-    }
-
-    for (int i = 0; i < z; i++) {
-        scanf("%d", &arr[i]);
-    }
-
-    int n = z;
-    int k = 3; // Number of steps to rotate
-
-    rotateRight(arr, n, k);
-    for (int i = 0; i < n; i++) {
-        printf("%d\n", arr[i]);
-    }
+    int n;
+    scanf("%d", &n);
     printf("\n");
-    free(arr);
+
+    // Declare the array with the specified size
+    int arr[n];
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d\n", &arr[i]);
+    }
+    int k;
+    scanf("%d", &k);
+    rotateArray(arr, n, k);
+    printArray(arr, n);
+
     return 0;
 }
+
