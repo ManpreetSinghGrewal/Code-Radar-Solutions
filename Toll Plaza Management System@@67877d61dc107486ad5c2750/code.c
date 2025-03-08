4bdf5cc06ff4853ff29031024f42a6e9ec67fd1d
Manpreet Singh <154874631+ManpreetSinghGrewal@users.noreply.github.com>
2025-03-08 06:05:32 +0530

    Solution to Toll Plaza Management System

--- Toll Plaza Management System@@67877d61dc107486ad5c2750/code.c
+++ Toll Plaza Management System@@67877d61dc107486ad5c2750/code.c
@@ -1,32 +1,32 @@
-// Your code here...
-// Your code here...
 #include <stdio.h>
 #include <string.h>
-struct Toll{
+
+struct Toll {
     char np[99];
     char name[99];
     int tax;    
 };
+
 int main() {
     int n;
-    scanf("%d",&n);
+    scanf("%d", &n);
     int ct = 0;
     int tt = 0;
     int bt = 0;
     struct Toll vech[n];
-    for(int i =0;i<n;i++){
-        scanf("%s %s %d",&vech[i].np,&vech[i].name,&vech[i].tax);
-        if(strcmp(vech[i],"Car") == 0){
+    
+    for (int i = 0; i < n; i++) {
+        scanf("%s %s %d", vech[i].np, vech[i].name, &vech[i].tax);  // Corrected line
+        if (strcmp(vech[i].name, "Car") == 0) {  // Using strcmp for string comparison
             ct = ct + vech[i].tax;
-        }
-        else if(strcmp(vech[i],"Truck") == 0){
+        } else if (strcmp(vech[i].name, "Truck") == 0) {
             tt = tt + vech[i].tax;
-        }
-        else{
+        } else {
             bt = bt + vech[i].tax;
         }
     }
     
-        printf("Cars: %d, Trucks: %d, Bikes: %d",ct,tt,bt);
-        printf("\n");
+    printf("Cars: %d, Trucks: %d, Bikes: %d\n", ct, tt, bt);
+    
+    return 0;
 }
