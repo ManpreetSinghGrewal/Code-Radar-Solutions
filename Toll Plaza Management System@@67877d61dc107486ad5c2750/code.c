4728ebcafe8bd50c18574a14c2f2a27c43d1d238
Manpreet Singh <154874631+ManpreetSinghGrewal@users.noreply.github.com>
2025-03-08 06:00:30 +0530

    Solution to Toll Plaza Management System

--- Toll Plaza Management System@@67877d61dc107486ad5c2750/code.c
+++ Toll Plaza Management System@@67877d61dc107486ad5c2750/code.c
@@ -9,20 +9,20 @@ struct Toll{
 int main() {
     int n;
     scanf("%d",&n);
-    float ct = 0;
-    float tt = 0;
-    float bt = 0;
+    int ct = 0;
+    int tt = 0;
+    int bt = 0;
     struct Toll vech[n];
     for(int i =0;i<n;i++){
         scanf("%s %s %d",&vech[i].np,&vech[i].name,&vech[i].tax);
         if(vech[i].name == "Car"){
-            ct = ct + float vech[i].tax;
+            ct = ct + vech[i].tax;
         }
         else if(vech[i].name == "Truck"){
-            tt = tt + float vech[i].tax;
+            tt = tt + vech[i].tax;
         }
         else{
-            bt = bt + float vech[i].tax;
+            bt = bt + vech[i].tax;
         }
     }
     for(int i =0;i<n;i++){
