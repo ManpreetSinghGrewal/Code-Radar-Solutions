9ef1645297a9abf72302187dfd82975d6f24c0a0
Manpreet Singh <154874631+ManpreetSinghGrewal@users.noreply.github.com>
2025-03-08 05:58:29 +0530

    Solution to Toll Plaza Management System

--- Toll Plaza Management System@@67877d61dc107486ad5c2750/code.c
+++ Toll Plaza Management System@@67877d61dc107486ad5c2750/code.c
@@ -21,7 +21,7 @@ int main() {
         else if(vech[i].name == "Truck"){
             tt = tt + vech[i].tax;
         }
-        else(vech[i].name == "Bike"){
+        else{
             bt = bt + vech[i].tax;
         }
     }
