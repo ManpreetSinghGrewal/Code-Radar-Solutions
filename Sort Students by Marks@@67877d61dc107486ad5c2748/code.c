c9346b84a9e4a8a7bcddb47d88e2a5299971d5f7
Manpreet Singh <154874631+ManpreetSinghGrewal@users.noreply.github.com>
2025-03-13 17:48:40 +0530

    Solution to Sort Students by Marks

--- Sort Students by Marks@@67877d61dc107486ad5c2748/code.c
+++ Sort Students by Marks@@67877d61dc107486ad5c2748/code.c
@@ -4,7 +4,7 @@
 int bubbleSort(int arr[], int n) {
     for (int i = 0; i < n - 1; i++) {
         for (int j = 0; j < n - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
+            if (arr[j].roll > arr[j + 1].roll) {
                 // Swap arr[j] and arr[j + 1]
                 int temp = arr[j];
                 arr[j] = arr[j + 1];
