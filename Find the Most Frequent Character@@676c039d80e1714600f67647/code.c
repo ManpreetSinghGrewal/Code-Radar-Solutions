4f3648b9d9b186076fefbc8d6874fde75a765e3d
Manpreet Singh <154874631+ManpreetSinghGrewal@users.noreply.github.com>
2025-03-17 14:49:14 +0530

    Solution to Find the Most Frequent Character

--- Find the Most Frequent Character@@676c039d80e1714600f67647/code.c
+++ Find the Most Frequent Character@@676c039d80e1714600f67647/code.c
@@ -1,22 +1,39 @@
-// Your code here...
 #include <stdio.h>
 #include <string.h>
-int main(){
+
+int main() {
     char str[1000];
-    int mc = 0;
-    char mf= '\0';
-    fgets(str,sizeof(str),stdin);
-    for(int i = 0;i<strlen(str);i++){
-        int c = 0;
-        for(int j = 0;j<strlen(str)-1;j++){
-            if(str[i] == str[j+1]){
+    int mc = 0; // Maximum count
+    char mf = '\0'; // Most frequent character
+
+    // Input the string
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    // Remove the newline character if present
+    str[strcspn(str, "\n")] = '\0';
+
+    int length = strlen(str); // Store the length of the string
+
+    for (int i = 0; i < length; i++) {
+        int c = 0; // Counter for the current character
+        for (int j = 0; j < length; j++) {
+            if (str[i] == str[j]) { // Compare all characters in the string
                 c++;
             }
         }
-        if(c > mc || (c == mc && str[i] < mf)){
+        // Update most frequent character and its count
+        if (c > mc || (c == mc && str[i] < mf)) {
             mc = c;
             mf = str[i];
-        } 
-    }printf("%c",mf);
+        }
+    }
 
+    if (mf != '\0') {
+        printf("%c", mf);
+    } else {
+        printf("No valid characters in the string.\n");
+    }
+
+    return 0;
 }
