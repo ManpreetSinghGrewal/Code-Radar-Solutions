7d412e3caf2b9f6a245ff5ce5b8217ba40bed31c
Manpreet Singh <154874631+ManpreetSinghGrewal@users.noreply.github.com>
2025-03-19 19:27:09 +0530

    Solution to Remove All Consonants from a String

--- Remove All Consonants from a String@@67877c59ddaeab38c8ad25b9/code.c
+++ Remove All Consonants from a String@@67877c59ddaeab38c8ad25b9/code.c
@@ -2,21 +2,31 @@
 #include <string.h>
 
 void printArray(char arr[], int size) {
-    for (int i = 1; i < size; i++) {
-            printf("%c",arr[i]);
+    for (int i = 0; i < size; i++) { // Start from 0
+        if (i > 0 && arr[i - 1] == ' ') {
+            printf(" %c", arr[i]);
+        } else {
+            printf("%c", arr[i]);
+        }
     }
 }
 
 int main() {
     char arr[100];
     fgets(arr, 100, stdin);
-    int n = strlen(arr) - 1;
-    char temp[n];
+
+    int n = strlen(arr);
+    if (arr[n - 1] == '\n') { // Remove trailing newline
+        arr[n - 1] = '\0';
+        n--;
+    }
+
+    char temp[100]; // Ensure temp has enough space
     int tempIndex = 0;
 
     for (int i = 0; i < n; i++) {
         if (arr[i] == 'a' || arr[i] == 'e' || arr[i] == 'i' || arr[i] == 'o' || arr[i] == 'u' ||
-            arr[i] == 'A' || arr[i] == 'E' || arr[i] == 'I' || arr[i] == 'O' || arr[i] == 'U' || arr[i] == ' ') {
+            arr[i] == 'A' || arr[i] == 'E' || arr[i] == 'I' || arr[i] == 'O' || arr[i] == 'U') {
             temp[tempIndex] = arr[i];
             tempIndex++;
         }
