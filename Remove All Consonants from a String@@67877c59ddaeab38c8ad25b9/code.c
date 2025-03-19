b1a9ab5d5c72dc69ce8716241270707823859c84
Manpreet Singh <154874631+ManpreetSinghGrewal@users.noreply.github.com>
2025-03-19 19:22:26 +0530

    Solution to Remove All Consonants from a String

--- Remove All Consonants from a String@@67877c59ddaeab38c8ad25b9/code.c
+++ Remove All Consonants from a String@@67877c59ddaeab38c8ad25b9/code.c
@@ -1,19 +1,5 @@
 #include <stdio.h>
 #include <string.h>
-
-void printArray(char arr[], int size) {
-    for (int i = 1; i < size; i++) {
-        if(arr[i-1] = ' '){
-            printf(" %c", arr[i]);
-        }
-        else{
-            printf("%c",arr[i]);
-        }
-        
-    }
-
-}
-
 int main() {
     char arr[100];
     fgets(arr, 100, stdin);
@@ -23,7 +9,7 @@ int main() {
 
     for (int i = 0; i < n; i++) {
         if (arr[i] == 'a' || arr[i] == 'e' || arr[i] == 'i' || arr[i] == 'o' || arr[i] == 'u' ||
-            arr[i] == 'A' || arr[i] == 'E' || arr[i] == 'I' || arr[i] == 'O' || arr[i] == 'U') {
+            arr[i] == 'A' || arr[i] == 'E' || arr[i] == 'I' || arr[i] == 'O' || arr[i] == 'U' || arr[i] = '\0') {
             temp[tempIndex] = arr[i];
             tempIndex++;
         }
