e7db79d1e44e8fb6f54f28ab81895df218acd8e5
Manpreet Singh <154874631+ManpreetSinghGrewal@users.noreply.github.com>
2025-03-20 23:28:59 +0530

    Solution to Convert to Uppercase

--- Convert to Uppercase@@676c039d80e1714600f67645/code.c
+++ Convert to Uppercase@@676c039d80e1714600f67645/code.c
@@ -12,7 +12,7 @@ int main() {
     for (int i = 0; input[i] != '\0'; i++) {
         if (isalpha(input[i])) { // Check if it's a letter
             char upper = toupper(input[i]); // Convert to uppercase
-            if (upper != 'A' && upper != 'E' && upper != 'I' && upper != 'O' && upper != 'U') {
+            if (upper != '0') {
                 output[j++] = upper; // Add to output if not a vowel
             }
         }
