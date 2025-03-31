323ffff6bb77ce5ab0b51a0d11f2509f3628784c
Manpreet Singh <154874631+ManpreetSinghGrewal@users.noreply.github.com>
2025-03-31 22:38:16 +0530

    Solution to Find the First Peak Element

--- Find the First Peak Element@@676c02002dbc8c3e7a280b6e/code.c
+++ Find the First Peak Element@@676c02002dbc8c3e7a280b6e/code.c
@@ -4,20 +4,22 @@ char* welcome() {
     return "Welcome to Code Radar!";
 }
 int findFirstPeek(int arr[], int n) {
-    if (n < 2) return -1; // not enough elements to find a peek
-
-    if (arr[0] > arr[1]) return arr[0]; // first element is a peek
-
-    for (int i = 1; i < n - 1; i++) {
-        if (arr[i] > arr[i - 1] && arr[i] > arr[i + 1]) {
-            return arr[i]; // found a peek element
+    if(n<2){
+        return -1;
+    }
+    if(arr[0] > arr[1]){
+        return  -1;
+    }
+    for(int i = 1;i<n-1;i++){
+        if(arr[i] >arr [i+1] && arr[i] > arr[i-1]){
+            return arr[i]
         }
     }
-
-    if (arr[n - 1] > arr[n - 2]) return arr[n - 1]; // last element is a peek
-
-    return -1; // no peek element found
-}
+    if(arr[n-1] >arr[n-2]){
+        return arr[n-1];
+    }
+    return -1;
+}   
 
 int main() {
     int a;
