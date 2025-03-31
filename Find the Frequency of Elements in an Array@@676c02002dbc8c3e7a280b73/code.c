e8e5c190682d83685b83a9864a6535dba2c2a654
Manpreet Singh <154874631+ManpreetSinghGrewal@users.noreply.github.com>
2025-04-01 00:12:44 +0530

    Solution to Find the Frequency of Elements in an Array

--- Find the Frequency of Elements in an Array@@676c02002dbc8c3e7a280b73/code.c
+++ Find the Frequency of Elements in an Array@@676c02002dbc8c3e7a280b73/code.c
@@ -1,29 +1,29 @@
 #include <stdio.h>
 
-void findFrequency(int arr[], int n) {
-    int freq[n]; 
-    for (int i = 0; i < n; i++) {
-        freq[i] = -1;
-    }
-    for (int i = 0; i < n; i++) {
-        int count = 1;
-        for (int j = i + 1; j < n; j++) {
-            if (arr[i] == arr[j]) {
-                count++;
-                freq[j] = 0;  
-            }
-        }
-        if (freq[i] != 0) {
-            freq[i] = count;
-        }
-    }
+// void findFrequency(int arr[], int n) {
+//     int freq[n]; 
+//     for (int i = 0; i < n; i++) {
+//         freq[i] = -1;
+//     }
+//     for (int i = 0; i < n; i++) {
+//         int count = 1;
+//         for (int j = i + 1; j < n; j++) {
+//             if (arr[i] == arr[j]) {
+//                 count++;
+//                 freq[j] = 0;  
+//             }
+//         }
+//         if (freq[i] != 0) {
+//             freq[i] = count;
+//         }
+//     }
     
-    for (int i = 0; i < n; i++) {
-        if (freq[i] != 0) {
-            printf("%d %d\n", arr[i], freq[i]);
-        }
-    }
-}
+//     for (int i = 0; i < n; i++) {
+//         if (freq[i] != 0) {
+//             printf("%d %d\n", arr[i], freq[i]);
+//         }
+//     }
+// }
 
 int main() {
     int n;
@@ -32,6 +32,19 @@ int main() {
     for (int i = 0; i < n; i++) {
         scanf("%d ", &arr[i]);
     }
-    findFrequency(arr, n);
+    for(int i =0;i<n;i++){
+        int c = 1;
+        for(int j=i+1;j<n;j++){
+            if(arr[i] == arr [j]){
+                c++;
+                arr[j] = -1;
+            }
+        }
+        if(arr[i] != -1){
+            printf("%d %d ",arr[i],c);
+        }
+    }
+
     return 0;
 }
+
