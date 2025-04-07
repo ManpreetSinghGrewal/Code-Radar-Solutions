7467223a8c7bd5992ce54a882469dbd31f2d1e66
Manpreet Singh <154874631+ManpreetSinghGrewal@users.noreply.github.com>
2025-04-08 02:04:32 +0530

    Solution to Check if a String is a Palindrome After Removing Spaces

--- Check if a String is a Palindrome After Removing Spaces@@67877c59ddaeab38c8ad25b7/code.c
+++ Check if a String is a Palindrome After Removing Spaces@@67877c59ddaeab38c8ad25b7/code.c
@@ -1,28 +1,35 @@
-// Your code here...
 #include <stdio.h>
 #include <string.h>
-int main(){
+
+int main() {
     char str[100];
-    fgets(str,sizeof(str),stdin);
-    str[strcspn(str, "\n")] = '\0';
-    for(int i =0;i< strlen(str);i++){
-        if(str[i] == ' '){
-            for(int j =i;j< strlen(str);j++){
-                str[j] = str[j+1];
-            }
+    fgets(str, sizeof(str), stdin);
+    str[strcspn(str, "\n")] = '\0'; // Remove trailing newline
+
+    // Removing spaces
+    int i, j = 0;
+    for (i = 0; str[i] != '\0'; i++) {
+        if (str[i] != ' ') {
+            str[j++] = str[i];
         }
     }
+    str[j] = '\0'; // Null terminate the modified string
+
+    // Reverse the string
     int l = strlen(str);
-    char temp[100];
-    int j= 0;
-    for(int i = l - 1;i>=0;i--){
+    char temp[l + 1]; // Ensure null termination
+    temp[l] = '\0';
+
+    for (i = l - 1, j = 0; i >= 0; i--, j++) {
         temp[j] = str[i];
-        j++;
     }
-    if(strcmp(temp,str) == 0){
-        printf("Yes");
+
+    // Check if palindrome
+    if (strcmp(temp, str) == 0) {
+        printf("Yes\n");
+    } else {
+        printf("No\n");
     }
-    else{
-        printf("No");
-    }
+
+    return 0;
 }
